bc819e7e7aaa8cdbbb18bb8d4de8696a8720587b
Shah Bagdadi <email.shanu@gmail.com>
2019-03-24 15:06:17 -0700

    Group Sum problem

---
+++ recursion/groupSum/README.md
@@ -0,0 +1,8 @@
+## Group Sum
+
+Given a set of integers and a target sum return true if a group  
+of numbers equal to the target sum else false.
+``` 
+ip = { 5, -1, 2,3 } and target = 4
+return true
+```
---
+++ recursion/groupSum/java/src/Main.java
@@ -0,0 +1,17 @@
+// T - O(?) where ? is ...
+// S - O(?) where ? is ...
+
+public class Main {
+
+    public boolean groupSum(int[] a, int t)
+    {
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+        Main m = new Main();
+        int[] ip = {4,-1,2,0,5,};
+        System.out.println(m.groupSum(ip,0));
+    }
+}
