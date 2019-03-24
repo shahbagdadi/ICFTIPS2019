7a4bdd1e2b9c0e582769c255a76d7eba5f54641e
Shah Bagdadi <email.shanu@gmail.com>
2019-03-24 15:02:23 -0700

    Coding test problem on 03/24/2019

---
+++ recursion/exam/java/src/Main.java
@@ -0,0 +1,17 @@
+// T - O(?) where ? is ...
+// S - O(?) where ? is ...
+
+public class Main {
+
+    private boolean hasEqualSubset(int[] ip) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Main m = new Main();
+        int[] ip1 = {1, 5, 11, 5};
+        System.out.println(m.hasEqualSubset(ip1));
+        int[] ip2 = {1, 2, 3, 5};
+        System.out.println(m.hasEqualSubset(ip2));
+    }
+}
