cdc5442719d15ec2b0844709946cd976673769fd
Shah Bagdadi <email.shanu@gmail.com>
2019-03-24 15:12:11 -0700

    Code covered in class

---
+++ tree/Identical Tree/README.md
@@ -0,0 +1,3 @@
+## Identical Tree
+
+Given 2 Trees with their root node , return true if both trees are identical.  
---
+++ tree/Identical Tree/java/src/Main.java
@@ -0,0 +1,14 @@
+// T - O(?) where ? is ...
+// S - O(?) where ? is ...
+
+public class Main {
+
+    public boolean isIdentical(Node n1, Node n2)
+    {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Main m = new Main();
+    }
+}
