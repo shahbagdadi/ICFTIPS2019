c6c0acf27ff4b3e83acf7db0597abb52a30b1566
Shah Bagdadi <email.shanu@gmail.com>
2019-03-24 15:36:59 -0700

    Coding test problem on 03/24/2019

---
+++ recursion/exam/IsPalindrome/README.md
@@ -0,0 +1,11 @@
+## IsPalindrome
+
+Given a string return true if it is a palindrome
+
+```
+ "a" -> true
+ "aaa" -> true
+ "aba" -> true
+ "abb" -> false
+
+```
---
+++ recursion/exam/IsPalindrome/java/src/Main.java
@@ -0,0 +1,14 @@
+// T - O(?) where ? is ...
+// S - O(?) where ? is ...
+
+public class Main {
+
+    public boolean IsPalindrome(String a) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Main m = new Main();
+        System.out.println(m.IsPalindrome("aaa"));
+    }
+}
