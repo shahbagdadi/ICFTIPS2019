5a7b78171edd2f0d88c1304e894425020214c896
Shah Bagdadi <email.shanu@gmail.com>
2019-03-29 20:53:05 -0700

    Min Stack

---
+++ llstq/class/MinStack/README.md
@@ -0,0 +1,23 @@
+## Minimum Stack
+
+
+```
+Design a stack that supports push, pop and retrieving the minimum element in constant time.
+
+push(x) -- Push element x onto stack.
+pop() -- Removes the element on top of the stack.
+
+getMin() -- Retrieve the minimum element in the stack.
+Example:
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin();   --> Returns -3.
+minStack.pop();
+
+minStack.getMin();   --> Returns -2.
+
+
+
+```
---
+++ llstq/class/MinStack/java/src/MinStack.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+// T - O(?) where ? is ...
+// S - O(?) where ? is ...
+public class MinStack {
+    
+    public MinStack() {
+
+    }
+
+    public void push(int x) {
+
+    }
+
+    public void pop() {
+
+    }
+
+
+
+    public int getMin() {
+
+    }
+
+    public static void main(String[] args) {
+
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        System.out.println(minStack.getMin());
+        minStack.pop();
+        minStack.getMin();
+    }
+}
