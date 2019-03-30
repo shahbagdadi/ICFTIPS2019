1eecee3feb3b1ddc8b1d8bb286b1ea02c228d092
Shah Bagdadi <email.shanu@gmail.com>
2019-03-29 19:26:25 -0700

    Add Integer LinkedList

---
+++ llstq/class/AddIntegerLinkedList/README.md
@@ -0,0 +1,11 @@
+## Add Integer Linked List
+
+Given two Integers LinkedList and add them
+
+```
+L1 : 2->4->3
+L2 : 5->6->4
+
+Result : 7->0->8
+
+```
---
+++ llstq/class/AddIntegerLinkedList/java/src/Main.java
@@ -0,0 +1,14 @@
+// T - O(?) where ? is ...
+// S - O(?) where ? is ...
+
+public class Main {
+
+    public Node addInteger(Node L1, Node L2) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Main m = new Main();
+        m.addInteger();
+    }
+}
