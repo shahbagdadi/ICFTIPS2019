3b12b1ccbe420fe405d82843e774e30861dbd947
khursheedma <khursheed_ma@yahoo.com>
2019-04-17 11:15:09 -0700

    valid brackets and minstack

--- llstq/class/MinStack/python/src/main.py
+++ llstq/class/MinStack/python/src/main.py
@@ -1,8 +1,88 @@
-# T - O(?) where ? is ...
-# S - O(?) where ? is ...
+"""
+Minimum Stack
+
+Design a stack that supports push, pop and retrieving the minimum element in constant time.
+
+push(x) -- Push element x onto stack.
+pop() -- Removes the element on top of the stack.
+
+getMin() -- Retrieve the minimum element in the stack.
+Example:
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin();   --> Returns -3.
+minStack.pop();
+
+minStack.getMin();   --> Returns -2.
+
+"""
+
+# T - O(?) where ? is ... O(1)
+# S - O(?) where ? is ... O(n)
+
+class Stack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if len(self.stack) < 1:
+            return None
+        return self.stack.pop()
+
+    def is_empty(self):
+        return self.stack == []
+
+    def peek(self):
+        if self.stack:
+            return self.stack[-1]
+
+
+class MinStack:
+
+    def __init__(self):
+        self.val = Stack()
+        self.min = Stack()
+
+    def push(self, i):
+
+        if self.min.is_empty() or self.min.peek() > i:
+            self.min.push(i)
+
+        self.val.push(i)
+
+    def pop(self):
+
+        val = self.min.pop()
+
+        if val == self.min.peek():
+            self.min.pop()
+
+        return val
+
+    def peek(self):
+        return self.val.peek()
+
+    def get_min(self):
+        return self.min.peek()
 
 def main():
-    pass
+    min_stack = MinStack()
+    min_stack.push(-2)
+    min_stack.push(0)
+    min_stack.push(-3)
+    print(min_stack.get_min())
+    # --> Returns - 3
+    min_stack.pop()
+
+    print(min_stack.get_min())
+    # --> Returns - 2
+
 
 if __name__ == '__main__':
   main()
--- llstq/class/ValidBrackets/python/src/main.py
+++ llstq/class/ValidBrackets/python/src/main.py
@@ -1,8 +1,63 @@
-# T - O(?) where ? is ...
-# S - O(?) where ? is ...
+"""
+Valid Brackets
+
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']',
+determine if the input string is valid.
+The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+"""
+
+# T - O(?) where ? is ...push, pop operations O(1)
+# S - O(?) where ? is ... O(size of a stack)
+
+class Stack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if len(self.stack) < 1:
+            return None
+        return self.stack.pop()
+
+    def is_empty(self):
+        return self.stack == []
+
+def is_balanced(str_var):
+
+    if str_var is None or len(str_var) < 2:
+        return False
+
+    stack = Stack()
+
+    for i in range(len(str_var)):
+
+        ch = str_var[i]
+
+        if ch == '[':
+            stack.push(']')
+        elif ch == '{':
+            stack.push('}')
+        elif ch == '(':
+            stack.push(')')
+        elif stack.is_empty() or stack.pop() != ch:
+            return False
+
+    return stack.is_empty()
 
 def main():
-    pass
+    print("Valid Brackets for a given string of characters, return True or False")
+    print("test case 1, input : ()")
+    print(is_balanced("()"))
+    print("test case 2, input : ()[]{}")
+    print(is_balanced("()[]{}"))
+    print("test case 3, input : (]")
+    print(is_balanced("(]"))
+    print("test case 4, input : ([)]")
+    print(is_balanced("([)]"))
+
 
 if __name__ == '__main__':
   main()
